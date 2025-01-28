5f4e2f0f49703623cb19fae71fb4f1e9761fb42b
ntp6647 <156668317+ntpsec6647@users.noreply.github.com>
2025-01-28 18:17:53 +0000

    adding my solution to the sumAll task

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,20 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let final = 0;
+    if(!Number.isInteger(start)||!Number.isInteger(end)){
+        return "ERROR"
+    }
+    if (start < 0 || end < 0){
+        return "ERROR"
+    }
+    if (start > end) {
+        const min = end;
+        end = start;
+        start = min;
+    }
+    for (let i = start; i <= end; i++) {
+        final = final + i;
+    } 
+    return final;
 
 };
 
