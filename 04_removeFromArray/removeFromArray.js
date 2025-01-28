cd7b9a08e1cb692afde6f583b6736f1e18669490
ntp6647 <156668317+ntpsec6647@users.noreply.github.com>
2025-01-28 16:33:14 +0000

    update with my solution to removeFromArray task

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
 const removeFromArray = function() {
+    let t;
+    const nArr = arguments[0];
+    for (let x=1; x<arguments.length; x++) {
+        t = arguments[x];
+        for(let i = nArr.length-1; i >=0; i--){
+            if(nArr[i] === t) {
+                nArr.splice(i, 1)
+            }
+        }
+    }
+    return nArr
 };
 
 // Do not edit below this line
