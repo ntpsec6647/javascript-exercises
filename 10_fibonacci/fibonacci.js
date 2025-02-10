05e96cf7c2978672c812e2edd67469c21d5aa793
ntp6647 <156668317+ntpsec6647@users.noreply.github.com>
2025-02-10 14:28:38 +0000

    Added my solutions to module 10 fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(x) {
+let arr = [1,1];
+const y = Number(x)
+if (y===0){
+    return 0;
+} else if (y < 0){
+    return "OOPS"
+} else {
+    for(let i=1; i<=y-2;i++){
+        arr.push(arr[i]+arr[i-1])
+    }
+    return arr[y-1];
+}
 };
 
 // Do not edit below this line
